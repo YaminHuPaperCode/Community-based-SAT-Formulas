42475bd04b83679ffcf3f6471c5f18b1f23f2285
YaminHuPaperCode <43240199+YaminHuPaperCode@users.noreply.github.com>
2018-10-07 15:13:53 +0800

    Add files via upload

---
+++ entity/Community.py
@@ -0,0 +1,7 @@
+# the structure of a community
+class Community:
+    def __init__(self, index):
+        # the index of this community
+        self.index = index
+        # the variables that this community contains
+        self.variableSet = set()
