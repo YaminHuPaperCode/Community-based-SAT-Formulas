51fa0a6634adf26dff5bf2544fbf226945c9a99c
YaminHuPaperCode <43240199+YaminHuPaperCode@users.noreply.github.com>
2018-10-07 15:22:55 +0800

    Delete Community.py

--- entity/Community.py
+++
@@ -1,7 +0,0 @@
-# the structure of a community
-class Community:
-    def __init__(self, index):
-        # the index of this community
-        self.index = index
-        # the variables that this community contains
-        self.variableSet = set()
