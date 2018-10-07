b4330c88ff7927dfb3415670c68f3df7f70c6711
YaminHuPaperCode <43240199+YaminHuPaperCode@users.noreply.github.com>
2018-10-07 15:22:41 +0800

    Delete Clause.py

--- entity/Clause.py
+++
@@ -1,8 +0,0 @@
-# the structure of a clause
-class Clause:
-    def __init__(self):
-        # the indexes of variables in a clause
-        self.variableIndexes = None
-        # the popularities of literals in a clause
-        # the literal x is in positive polarity; the literal neg(x) is in negative polarity
-        self.polarities = None
